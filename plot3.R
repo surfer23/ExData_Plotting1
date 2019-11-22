32eaf0243017339a6944b7a34b1860311d9d86a4
Hugo Retief <hugoretief@gmail.com>
2019-11-22 08:31:21 +0200

    Final Submission

--- plot3.R
+++ plot3.R
@@ -18,10 +18,17 @@ data_sub <- cbind(date_time, data_sub)
 data_sub$date_time <- as.POSIXct(date_time)
 
 #Create the PNG with the required specifications
-png(filename = 'plot2.png', width = 480, height = 480, units='px')
+png(filename = 'plot3.png', width = 480, height = 480, units='px')
 
 #Plot the data which will be added to the PNG
-with(data_sub, plot(data_sub$Global_active_power~data_sub$date_time, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+with(data_sub, {
+  plot(Sub_metering_1~date_time, type="l", 
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~date_time,col='Red')
+  lines(Sub_metering_3~date_time,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
 
 #Release the device
 dev.off()
--- plot4.R
+++ plot4.R
@@ -18,10 +18,26 @@ data_sub <- cbind(date_time, data_sub)
 data_sub$date_time <- as.POSIXct(date_time)
 
 #Create the PNG with the required specifications
-png(filename = 'plot2.png', width = 480, height = 480, units='px')
+png(filename = 'plot4.png', width = 480, height = 480, units='px')
+
+#Define the number of rows and columns for plotting more than one chart into the PNG
+par(mfrow=c(2,2))
 
 #Plot the data which will be added to the PNG
-with(data_sub, plot(data_sub$Global_active_power~data_sub$date_time, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+with(data_sub, {
+  plot(Global_active_power~date_time, type="l", 
+       ylab="Global Active Power", xlab="")
+  plot(Voltage~date_time, type="l", 
+       ylab="Voltage", xlab="datetime")
+  plot(Sub_metering_1~date_time, type="l", 
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~date_time,col='Red')
+  lines(Sub_metering_3~date_time,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~date_time, type="l", 
+       ylab="Global_rective_power",xlab="datetime")
+})
 
 #Release the device
 dev.off()
